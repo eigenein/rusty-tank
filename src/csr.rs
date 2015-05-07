af63c7d17294073b0733973558dcd6216b5f9a00
Pavel Perestoronin <eigenein@gmail.com>
2015-05-07 04:03:35 +0300

    Filling up matrix.

--- src/csr.rs
+++ src/csr.rs
@@ -1,1 +1,89 @@
-//! Compressed Sparse Row implementation.
+//! Compressed Sparse Row (and Compressed Sparse Column) implementation.
+
+#[derive(Debug)]
+pub struct IndexedValue<I, V> {
+    index: I,
+    value: V
+}
+
+#[derive(Debug)]
+pub struct CompressedStorage<I, V> {
+    values: Vec<IndexedValue<I, V>>,
+    pointers: Vec<usize>
+}
+
+impl<I, V> CompressedStorage<I, V> {
+    pub fn new() -> CompressedStorage<I, V> {
+        CompressedStorage { values: Vec::new(), pointers: Vec::new() }
+    }
+
+    /// Starts a new row (column).
+    ///
+    /// Ensure that you have started a new row after the very last value.
+    pub fn start(&mut self) {
+        self.pointers.push(self.values.len());
+    }
+
+    /// Adds a new value to the current row (column).
+    pub fn next(&mut self, index: I, value: V) {
+        self.values.push(IndexedValue { value: value, index: index });
+    }
+
+    /// Gets value count.
+    pub fn len(&self) -> usize {
+        self.values.len()
+    }
+
+    /// Gets a slice to row (column) slice.
+    pub fn get_slice(&self, index: usize) -> &[IndexedValue<I, V>] {
+        &self.values[self.pointers[index]..self.pointers[index + 1]]
+    }
+}
+
+#[test]
+fn test_start() {
+    let mut storage = CompressedStorage::new();
+    storage.start();
+    storage.next(0, 1.0);
+    storage.next(2, 2.0);
+    storage.start();
+    assert_eq!(storage.pointers.len(), 2);
+    assert_eq!(storage.pointers[0], 0);
+    assert_eq!(storage.pointers[1], 2);
+}
+
+#[test]
+fn test_next() {
+    let mut storage = CompressedStorage::new();
+    storage.start();
+    storage.next(0, 1.0);
+    storage.next(2, 2.0);
+    storage.next(5, 3.0);
+    storage.start();
+    assert_eq!(storage.values.len(), 3);
+}
+
+#[test]
+fn test_len() {
+    let mut storage = CompressedStorage::new();
+    storage.start();
+    storage.next(0, 1.0);
+    storage.next(2, 2.0);
+    storage.start();
+    assert_eq!(storage.len(), 2);
+}
+
+#[test]
+fn test_get_slice() {
+    let mut storage = CompressedStorage::new();
+    storage.start();
+    storage.next(0, 1.0);
+    storage.start();
+    storage.next(2, 2.0);
+    storage.next(5, 3.0);
+    storage.start();
+    storage.next(1, 7.0);
+    storage.start();
+    assert_eq!(storage.get_slice(1).len(), 2);
+    assert_eq!(storage.get_slice(1)[0].index, 2);
+}
--- src/main.rs
+++ src/main.rs
@@ -7,37 +7,45 @@ use std::io::{BufReader, Read};
 
 extern crate time;
 
+mod csr;
 mod protobuf;
 mod stats;
 mod svd;
 
+#[allow(dead_code)]
 fn main() {
     let mut input = get_input();
-    read_stats(&mut input);
+    let storage = read_stats(&mut input);
 }
 
 /// Reads statistics file.
-fn read_stats<R: Read>(input: &mut R) {
+fn read_stats<R: Read>(input: &mut R) -> csr::CompressedStorage<u16, f32> {
     use time::now;
 
     let start_time = time::now();
     let mut last_progress_report = start_time;
+    let mut storage = csr::CompressedStorage::new();
     println!("Reading started at {}.", start_time.ctime());
 
     for i in 1.. {
         if now().tm_sec != last_progress_report.tm_sec {
-            println!("Reading | {} accounts", i);
+            println!("Reading | {} accounts | {} values", i, storage.len());
             last_progress_report = now();
         }
         match stats::read_account(input) {
             Some(account) => {
-                // TODO.
+                storage.start();
+                for tank in account.tanks {
+                    storage.next(tank.id, tank.wins as f32 / tank.battles as f32);
+                }
             }
             None => break
         }
     }
 
-    println!("Read in {}s.", (now() - start_time).num_seconds());
+    storage.start();
+    println!("Read {} values in {}s.", storage.len(), (now() - start_time).num_seconds());
+    storage
 }
 
 /// Gets statistics input.
