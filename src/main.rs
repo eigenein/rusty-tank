0705a8e709834e476402b5f803bb0e381c6ec323
Pavel Perestoronin <eigenein@gmail.com>
2015-05-18 04:40:13 +0300

    Reimplemented evaluate(). MAX_DEVIATION instead of MIN_BATTLES.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@
 
 use std::fs::File;
 use std::io::{BufReader, Read};
-use std::collections::HashMap;
 
 extern crate rand;
 extern crate time;
@@ -17,33 +16,33 @@ mod protobuf;
 mod stats;
 mod svd;
 
-/// Minimum battle count to train with.
-const MIN_BATTLES: u32 = 10;
+/// Maximum deviation of tank rating from 0.5.
+const MAX_DEVIATION: f64 = 0.05;
 /// SVD feature count.
-const FEATURE_COUNT: usize = 2;
+const FEATURE_COUNT: usize = 4;
 /// Learning rate.
 const RATE: f64 = 0.1;
 /// Regularization parameter.
-const LAMBDA: f64 = 2.0;
+const LAMBDA: f64 = 8.0;
 
 #[allow(dead_code)]
 fn main() {
     let mut input = get_input();
     let encyclopedia = encyclopedia::Encyclopedia::new();
-    let (train_table, test_table, overall_rating) = read_stats(&mut input, &encyclopedia);
+    let (train_table, test_table) = read_stats(&mut input, &encyclopedia);
     let mut model = svd::Model::new(train_table.row_count(), encyclopedia.len(), FEATURE_COUNT);
     println!("Initial evaluation.");
-    let initial_train_score = 100.0 * evaluate(&model, &train_table, &overall_rating);
-    println!("Train score: {0:.2}.", initial_train_score);
-    let initial_test_score = 100.0 * evaluate(&model, &test_table, &overall_rating);
-    println!("Test score: {0:.2}.", initial_test_score);
-    train(&mut model, &train_table, &test_table, &overall_rating);
+    let (train_avg, train_max) = evaluate(&model, &train_table);
+    println!("Train | avg {:.6} | max {:.6}.", train_avg, train_max);
+    let (test_avg, test_max) = evaluate(&model, &test_table);
+    println!("Test  | avg {:.6} | max {:.6}.", test_avg, test_max);
+    train(&mut model, &train_table, &test_table);
 }
 
 /// Reads statistics file.
 ///
 /// Returns train rating table and test rating table.
-fn read_stats<R: Read>(input: &mut R, encyclopedia: &encyclopedia::Encyclopedia) -> (csr::Csr, csr::Csr, HashMap<usize, f64>) {
+fn read_stats<R: Read>(input: &mut R, encyclopedia: &encyclopedia::Encyclopedia) -> (csr::Csr, csr::Csr) {
     use rand::{Rng, thread_rng};
 
     use time::now;
@@ -53,7 +52,8 @@ fn read_stats<R: Read>(input: &mut R, encyclopedia: &encyclopedia::Encyclopedia)
 
     let mut train_table = csr::Csr::new();
     let mut test_table = csr::Csr::new();
-    let mut overall_rating = HashMap::new();
+
+    let mut non_empty_account_count = 0;
 
     println!("Reading started at {}.", start_time.ctime());
 
@@ -68,34 +68,36 @@ fn read_stats<R: Read>(input: &mut R, encyclopedia: &encyclopedia::Encyclopedia)
         train_table.start();
         test_table.start();
 
+        let mut non_empty = false;
+
         match stats::read_account(input) {
             Some(account) => {
-                let mut account_wins = 0;
-                let mut account_battles = 0;
                 for tank in account.tanks {
-                    if tank.battles < MIN_BATTLES {
+                    let tank_rating = tank.wins as f64 / tank.battles as f64;
+                    if (tank_rating - 0.5).abs() > MAX_DEVIATION {
                         continue;
                     }
-                    let tank_rating = tank.wins as f64 / tank.battles as f64;
+                    non_empty = true;
                     (if !rng.gen_weighted_bool(4) {
-                        account_wins += tank.wins;
-                        account_battles += tank.battles;
                         &mut train_table
                     } else {
                         &mut test_table
                     }).next(encyclopedia.get_column(tank.id), tank_rating);
                 }
-                if account_battles != 0 {
-                    overall_rating.insert(train_table.row_count(), account_wins as f64 / account_battles as f64);
-                }
             }
             None => break
+        }
+
+        if non_empty {
+            non_empty_account_count += 1;
         }
     }
 
     println!("Read {1} train and {2} test values in {0:.1}s.", get_seconds(start_time), train_table.len(), test_table.len());
+    println!("Non-empty accounts: {0} ({1:.2}%).",
+        non_empty_account_count, 100.0 * non_empty_account_count as f32 / train_table.row_count() as f32);
 
-    (train_table, test_table, overall_rating)
+    (train_table, test_table)
 }
 
 /// Gets statistics input.
@@ -108,7 +110,7 @@ fn get_input() -> BufReader<File> {
 }
 
 /// Trains the model.
-fn train(model: &mut svd::Model, train_table: &csr::Csr, test_table: &csr::Csr, overall_rating: &HashMap<usize, f64>) {
+fn train(model: &mut svd::Model, train_table: &csr::Csr, test_table: &csr::Csr) {
     use std::f64;
     use time::now;
 
@@ -119,14 +121,15 @@ fn train(model: &mut svd::Model, train_table: &csr::Csr, test_table: &csr::Csr, 
     let mut previous_rmse = f64::INFINITY;
     for step in 0.. {
         let rmse = model.make_step(RATE, LAMBDA, train_table);
-        let train_score = 100.0 * evaluate(model, &train_table, &overall_rating);
-        let test_score = 100.0 * evaluate(model, &test_table, &overall_rating);
+        let (train_avg, train_max) = evaluate(model, &train_table);
+        let (test_avg, test_max) = evaluate(model, &test_table);
         let drmse = rmse - previous_rmse;
         println!(
-            "#{0} | {3:.3} sec | RMSE: {1:.6} | dE: {2:.7} | train: {4:.3} | test: {5:.3}",
-            step, rmse, drmse, get_seconds(start_time) / (step as f32 + 1.0), train_score, test_score,
+            "#{0} | {3:.3} sec | RMSE: {1:.6} | dE: {2:.7} | train: {4:.6} ({5:.6}) | test: {6:.6} ({7:.6})",
+            step, rmse, drmse, get_seconds(start_time) / (step as f32 + 1.0),
+            train_avg, train_max, test_avg, test_max,
         );
-        if drmse.abs() < 0.000001 {
+        if drmse.abs() < 0.0000001 {
             break;
         }
         previous_rmse = rmse;
@@ -136,23 +139,19 @@ fn train(model: &mut svd::Model, train_table: &csr::Csr, test_table: &csr::Csr, 
 }
 
 /// Evaluates the model.
-fn evaluate(model: &svd::Model, table: &csr::Csr, overall_rating: &HashMap<usize, f64>) -> f64 {
-    let mut value_count = 0;
-    let mut true_count = 0;
-    for row_index in 0..(table.row_count() - 1) {
-        let account_rating = match overall_rating.get(&row_index) {
-            Some(&account_rating) => account_rating,
-            None => continue,
-        };
+fn evaluate(model: &svd::Model, table: &csr::Csr) -> (f64, f64) {
+    let mut error_sum = 0.0;
+    let mut error_max: f64 = 0.0;
+
+    for row_index in 0..table.row_count() {
         for actual_value in table.get_row(row_index) {
-            value_count += 1;
-            let predicted_value = model.predict(row_index, actual_value.column);
-            if (predicted_value > account_rating) == (actual_value.value > account_rating) {
-                true_count += 1;
-            }
+            let error = (actual_value.value - model.predict(row_index, actual_value.column)).abs();
+            error_sum += error;
+            error_max = error_max.max(error);
         }
     }
-    true_count as f64 / value_count as f64
+
+    (error_sum / table.len() as f64, error_max)
 }
 
 /// Gets seconds elapsed since the specified time.
