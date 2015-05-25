cf9cf0d8c5b0ed015fc9b8e6b136d234f86860fb
Pavel Perestoronin <eigenein@gmail.com>
2015-05-25 04:00:44 +0300

    CCF: read stats.

--- src/ccf.rs
+++ src/ccf.rs
@@ -1,8 +1,21 @@
 //! Clustering-based collaboration filtering for Tankopoisk.
 
+extern crate rand;
+extern crate time;
+
 mod corr;
 mod csr;
+mod encyclopedia;
+mod helpers;
+mod protobuf;
+mod stats;
+
+/// Minimum battles count.
+const MIN_BATTLES: u32 = 10;
 
 #[allow(dead_code)]
 fn main() {
+    let mut input = helpers::get_input();
+    let encyclopedia = encyclopedia::Encyclopedia::new();
+    let (train_table, test_table) = helpers::read_stats(&mut input, MIN_BATTLES, &encyclopedia);
 }
