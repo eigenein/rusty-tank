08b86ac09f234bc3ef6cf310769a59d2f6039b7c
Pavel Perestoronin <eigenein@gmail.com>
2015-06-01 00:29:33 +0300

    Slope One stub.

--- src/slopeone.rs
+++ src/slopeone.rs
@@ -19,7 +19,13 @@ struct Model {
 }
 
 impl Model {
+    pub fn new(column_count: usize) -> Model {
+        Model { average_difference: HashMap::new(), rating_count: vec![0; column_count] }
+    }
 
+    pub fn train(&mut self, matrix: &csr::Csr) {
+        //
+    }
 }
 
 impl helpers::AbstractModel for Model {
@@ -30,5 +36,17 @@ impl helpers::AbstractModel for Model {
 
 #[allow(dead_code)]
 fn main() {
-    //
+    let (encyclopedia, train_matrix, test_matrix) = helpers::get_stats(MIN_BATTLES, helpers::identity);
+    println!("Training.");
+    let mut model = Model::new(encyclopedia.len());
+    model.train(&train_matrix);
+    println!("Evaluating.");
+    let train_error = helpers::evaluate(&model, &train_matrix, &train_matrix, helpers::identity);
+    println!("Train error: {0:.6}.", train_error);
+    let test_error = helpers::evaluate(&model, &train_matrix, &test_matrix, helpers::identity);
+    println!("Test error: {0:.6}.", test_error);
+    let error_distribution = helpers::evaluate_error_distribution(&model, &train_matrix, &test_matrix, helpers::identity);
+    println!("Test error distribution:");
+    println!("------------------------");
+    helpers::print_error_distribution(error_distribution);
 }
